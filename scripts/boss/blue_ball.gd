26adeb5d108541238631ff57ee8b52c28cdba41e
ArthurWithH <arthurdealmeidaelias@gmail.com>
2025-05-30 12:16:50 -0300

    alterações boss

--- scripts/boss/blue_ball.gd
+++ scripts/boss/blue_ball.gd
@@ -1,7 +1,9 @@
-extends Node2D
+# BlueBall.gd  OU  RedBall.gd
+extends Area2D
 
-@export var speed: float = 100.0
+var boss_ref: Node2D = null
 var target_player_position: Vector2
+@export var speed: float = 100.0
 
 func set_player_position(pos: Vector2):
 	target_player_position = pos
@@ -11,3 +13,6 @@ func _process(delta):
 		return
 	var direction = (target_player_position - global_position).normalized()
 	global_position += direction * speed * delta
+
+func _ready():
+	print("BlueBall spawnada em: ", global_position)
--- scripts/boss/boss.gd
+++ scripts/boss/boss.gd
@@ -1,13 +1,42 @@
 extends Node2D
 
+@export var blue_ball_scene: PackedScene
+@export var red_ball_scene: PackedScene
 @export var player_path: NodePath = "Player"
+
+var balls := []
 var player_ref: Node2D = null
 
 func _ready():
 	player_ref = get_node(player_path)
+	spawn_balls()
+
+func spawn_balls():
+	print("Chamando spawn_balls") # 👈 Adicione este print!
+	
+	if blue_ball_scene:
+		var blue_ball = blue_ball_scene.instantiate()
+		print("Instanciando blue_ball:", blue_ball) # 👈
+		blue_ball.global_position = global_position + Vector2(50, 0)
+		blue_ball.boss_ref = self
+		get_tree().current_scene.add_child(blue_ball)
+		balls.append(blue_ball)
+	else:
+		print("blue_ball_scene está nulo!") # 👈
+
+	if red_ball_scene:
+		var red_ball = red_ball_scene.instantiate()
+		print("Instanciando red_ball:", red_ball) # 👈
+		red_ball.global_position = global_position + Vector2(-50, 0)
+		red_ball.boss_ref = self
+		get_tree().current_scene.add_child(red_ball)
+		balls.append(red_ball)
+	else:
+		print("red_ball_scene está nulo!") # 👈
 
 func _process(delta):
-	if player_ref:
-		var player_pos = player_ref.global_position
-		for bola in get_tree().get_nodes_in_group("boss_balls"):
-			bola.set_player_position(player_pos)
+	if not player_ref:
+		return
+	for bola in balls:
+		if bola:
+			bola.set_player_position(player_ref.global_position)
