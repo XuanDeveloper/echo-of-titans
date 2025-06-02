ee85df73300149286866948b1be453273186409f
Alfredo Pina <83417934+Alfr3d022@users.noreply.github.com>
2025-06-01 21:07:11 -0300

    Adicionado porta boss

---
+++ scripts/tutorialHud/fase_1.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var wall_move = $wallMove
+@onready var wall_trigger = $wallTrigger
+@onready var boss = $Boss
+
+func _ready():
+	wall_move.enabled = false
+	wall_trigger.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node) -> void:
+	print("Colidiu com algo: ", body.name)
+	if body.is_in_group("player"):
+		print("Player colidiu com o gatilho!")
+		wall_move.enabled = true
